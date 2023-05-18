b1b8b025a60ba36ba853aa5499c197953da191da
Vamsi Krishna Ummadi <vamsiu@italentdigital.com>
2023-05-18 15:04:49 +0530

    sample-react

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,12 +1,16 @@
-import React from 'react'
+import React, {useState} from 'react'
 import Header from './Header'
 
 const Home = () => {
+
+    const [number, setNumber] = (0)
+
   return (
     <div>
       <Header />
       <div className='body'>
         <h1>Welcome to Home Page</h1>
+        <button type="button" onClick={(prev) => setNumber(number +1 )}>add</button>
       </div>
     </div>
   )
